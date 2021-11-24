e24c8beb276eddceec451c20dea9035a61141cfc
LukaGrigic <luka.grigic94@gmail.com>
2021-11-24 07:15:22 +0100

    Create githubprimjer.sql

---
+++ githubprimjer.sql
@@ -0,0 +1,9 @@
+drop database if exists githubprimjer;
+create database githubprimjer;
+use database githubprimjer
+
+create table (
+    sifra int not null primary key auto_increment,
+    naziv varchar (50) not null
+);
+
