64ef355c4601568edf4b638ba077cfa3d9055ff9
LukaGrigic <luka.grigic94@gmail.com>
2021-11-24 08:44:09 +0100

    ERA.PNG

--- githubprimjer.sql
+++ githubprimjer.sql
@@ -1,8 +1,8 @@
 drop database if exists githubprimjer;
 create database githubprimjer character set utf8;
-use database githubprimjer
+use githubprimjer;
 
-create table (
+create table git (
     sifra int not null primary key auto_increment,
     naziv varchar (50) not null
 );
